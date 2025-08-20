146d9ddf0ec6e7d85e73333db9bc6ff9a2d2c990
jeddyhub <eddyjillian@gmail.com>
2025-08-20 11:07:25 -0700

    jne - changed to a src directory, deleted unnecessary files, cleaned up pyproject.toml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ extensions = [
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+sys.path.insert(0, os.path.abspath('../../src'))
 
 
 templates_path = ['_templates']
